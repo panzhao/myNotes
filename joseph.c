fa7594ad1adca9b8a35eb8947eb1a36a3881bddd
Mr.Zhao <zp88_zjl00@163.com>
2011-07-28 23:15:56 +0800

    joseph circle

---
+++ joseph.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+int joseph(int peop_num, int kick_num)
+{
+    int store_peop[peop_num];
+    int i = 0;
+
+    /*循环对函数数据进行初始化*/
+    for (i = 0; i < peop_num; i++)
+    {
+        store_peop[i] = 0;
+    }
+
+    int mark = 0; /*用来标记数数*/
+    int count = 0; /*用来计算被踢出的人的总数*/
+    
+    i = 0;
+
+    while (count < peop_num - 1)
+    {
+        if (store_peop[i] == 0)
+	{
+	    mark++;
+	}
+
+	if (mark == kick_num)
+	{
+	    store_peop[i] = 1;
+	    mark = 0;
+	    count++;
+	}
+        
+	i++;
+	i = i % peop_num;
+    }
+
+    for (i = 0; i < peop_num; i++)
+    {
+        if (store_peop[i] != 1)
+	{
+	    printf("The last people is %d\n", i + 1);
+	}
+    }
+}
+
+int main()
+{
+    int peop_num = 1000;
+    int kick_num = 5;
+
+    joseph(peop_num, kick_num);
+
+    return 0;
+}
