d1f78ec237c1aab2e5fb3f101c5196d5c999ec70
Mr.Zhao <zp88_zjl00@163.com>
2011-07-29 17:19:34 +0800

    处理最开始是空格， 多个空格以及末尾有空格的情况

--- modify_shell.c
+++ modify_shell.c
@@ -2,34 +2,33 @@
 
 int main()
 {
-    char source[128] = "bei     jing               huan ying nin!";
+    char source[128] = "    bei     jing               huan ying nin!    ";
     int argc = 0;
     char *argv[16];
     char *str = source;
     
     int i = 0;
     
-    argv[i] = str;
-    argc++;
-
-    int mark = 0;
-
     while (*str != '\0')
     {
 	if (*str == ' ')
 	{  
-	   argc++;
-	   i++;
-	   
+	 
 	   while (*str == ' ')
 	   {
 	       str++;
-	       mark++;
 	   }
+           
+	   if(*str == '\0')
+	   {
+	       break;
+	   }
+
+	   argc++;
        
 	   *--str = '\0';
           
-	   argv[i] = ++str;
+	   argv[i++] = ++str;
 
 	   continue;
 	}
