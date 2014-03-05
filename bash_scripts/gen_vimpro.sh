81b7e834144b46c6ab4871e881790c21babdd22f
zhaopan <zhaopan@cmos.net>
2014-03-05 18:32:51 +0800

    modify gen_vimpro.sh

--- bash_scripts/gen_vimpro.sh
+++ bash_scripts/gen_vimpro.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 #cscope
-touch Session.vim
+if [ ! -f Session.vim ]; then
+    echo "create new Session.vim ok"
+    touch Session.vim
+else 
+    echo "has Session.vim, no need to create"
+fi 
+
+#find all source file
 find `pwd` -name "*.cpp" -o -name "*.h" -o -name "*.c" > cscope.files;
-echo 'create cscope.out';
 cscope -bqk -i cscope.files 
-echo "cscope.files ok\n";
+echo "cscope.files ok";
+
 
 #ctags
-echo "create ctasg"
-ctags -R
-echo "ctags ok";
+if [ ! -f tags ]; then
+    echo "create new tags" 
+else
+    rm tags
+    echo "rm old ctags ok";
+fi    
+
+ctags -R --c++-kinds=+p --fields=+iaS --extra=+q
 
 #tags profile
-echo "create curProFile.vim";
-path=`pwd`;
-string="set tags=`pwd`/tags";
-string1="set path+=`pwd`/**/*"
-echo "$string" > curProFile.vim;
-echo "$string1" >> curProFile.vim;
-echo "curProFile.vim ok \n";
+if [ ! -f curProFile.vim ]; then
+    path=`pwd`;
+    string="set tags+=`pwd`/tags";
+    string1="set path+=`pwd`/**/*"
+    echo "$string" > curProFile.vim;
+    echo "$string1" >> curProFile.vim;
+    echo "curProFile.vim ok";
+else
+   echo  "curProFile.vim exist, no need to create"
+fi
+
+# 生成lookupfile索引文件
 lookupfile.sh
