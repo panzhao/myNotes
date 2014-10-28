f2ad30bcd772697a940deae1b5345a0b28b6023b
zhaopan <zhaopan@cmos.net>
2014-10-28 10:50:08 +0800

    [Others] modify

--- bash_scripts/du.sh
+++ bash_scripts/du.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-du --max-depth=1 -h $1
+#du --max-depth=1 -h $1
+du -BM --max-depth=0 $1
--- bash_scripts/start_server.sh
+++ bash_scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pid=$(adb shell ps|awk '/com.binfen.tvbrowser/{print $2}');
+pid=$(adb shell ps|awk '/com.android.browser/{print $2}');
 adb shell /system/bin/gdbserver :5039 --attach $pid;
