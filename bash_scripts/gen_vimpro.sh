20efed60a1dd1c42f9f278fb2782e9186b9d87a9
pan_zhao_dev <zhaopan@syberos.com>
2015-01-09 11:15:15 +0800

    [Ginkgo-P]add delswp.sh and add bundle [Bug] [Solution] [Feature] [Others]

--- bash_scripts/gen_vimpro.sh
+++ bash_scripts/gen_vimpro.sh
@@ -21,19 +21,26 @@ else
     echo "rm old ctags ok";
 fi    
 
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q
+#ctags -R --c++-kinds=+p --fields=+iaS --extra=+q
+
+if [ ! -f .ycm_extra_conf.py ]; then
+    echo "--------------------------"
+    cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py ./
+else 
+    echo ".ycm_extra_conf.py exist"
+fi
 
 #tags profile
-if [ ! -f curProFile.vim ]; then
-    path=`pwd`;
-    string="set tags+=`pwd`/tags";
-    string1="set path+=`pwd`/**/*"
-    echo "$string" > curProFile.vim;
-    echo "$string1" >> curProFile.vim;
-    echo "curProFile.vim ok";
-else
-   echo  "curProFile.vim exist, no need to create"
-fi
+#if [ ! -f curProFile.vim ]; then
+#    path=`pwd`;
+#    string="set tags+=`pwd`/tags";
+#    string1="set path+=`pwd`/**/*"
+#    echo "$string" > curProFile.vim;
+#    echo "$string1" >> curProFile.vim;
+#    echo "curProFile.vim ok";
+#else
+#   echo  "curProFile.vim exist, no need to create"
+#fi
 
 # 生成lookupfile索引文件
 lookupfile.sh
